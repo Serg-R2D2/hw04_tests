9f43a515c3da881b8d628972f70f76533d598f4c
Serg <srkononenko@mail.ru>
2020-07-03 01:48:40 +0300

    Done

--- posts/tests.py
+++ posts/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import reverse
 from django.test import TestCase
 from django.test import Client
-from .models import Post
+from .models import Post, Group
 
 
 class TestMethods(TestCase):
@@ -10,15 +10,21 @@ class TestMethods(TestCase):
         self.client = Client()
         self.user = User.objects.create_user(
                         username="User", 
-                        email="User@skynet.com", 
-                        password="User"
-                        )
+                            email="User@skynet.com", 
+                                password="User"
+                                )
+        self.group = Group.objects.create(
+                        slug="test4posts", 
+                            title="Test Group",
+                                description="Test Descr."
+                                )
         self.post = Post.objects.create(
             text=f"А теперь ты приходишь и говоришь: "
             f"Дон Корлеоне, мне нужна справедливость." 
             f"Но ты не просишь с уважением, не предлагаешь дружбу, "
             f"даже не думаешь обратиться ко мне — крёстный.", 
-            author=self.user)
+                author=self.user,
+                    group = self.group)
         
     def test_Profile(self):
         """Проверка создания персональной страницы после регистрации"""
@@ -40,8 +46,21 @@ class TestMethods(TestCase):
     def test_DispPost(self):
         """После публикации поста новая запись появляется на главной странице сайта (index), """
         """на персональной странице пользователя (profile), и на отдельной странице поста (post)"""
+        def check_content(self):
+            response = self.client.get("")
+            if Paginator in response.context:
+                self.assertEqual(response.context['Page'].user, self.user)
+                self.assertEqual(response.context['Page'].text, self.text)
+                self.assertEqual(response.context['Page'].group, self.group.id)
+            else:
+                self.assertEqual(response.context['post'].user, self.user)
+                self.assertEqual(response.context['post'].text, self.text)
+                self.assertEqual(response.context['post'].group, self.group.id)
+
+
         response = self.client.get("")
         self.assertContains(response, self.post.text, count=1)
+        self.assertContains(response, self.post.group.id)
         profile_url = reverse('profile', args=(self.post.author,))
         response1 = self.client.get(profile_url)
         self.assertContains(response, self.post.text, count=1)
