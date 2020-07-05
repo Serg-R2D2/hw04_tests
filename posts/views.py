d3b5c3ec2ff90d6bc05fc3ae4aafdec8023bb170
Serg <srkononenko@mail.ru>
2020-07-05 19:40:38 +0300

    Done

--- posts/views.py
+++ posts/views.py
@@ -29,7 +29,7 @@ def group_posts(request, slug):
     return render(
         request, 
         "group.html", 
-        {'page': page, 'paginator': paginator}
+        {'page': page, 'paginator': paginator, 'group': group}
         )
 
 @login_required
